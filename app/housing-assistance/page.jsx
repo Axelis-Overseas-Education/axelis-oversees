bd3343bce4bdf61f1216a337a617f5ed1f9a9e89
vicky86513 <rvicky86513@gmail.com>
2024-01-13 22:46:02 +0530

    housing assistance

---
+++ app/housing-assistance/page.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import styles from "app/lor_page/lor.module.css";
+const HousingAssistsnce = () => {
+  return (
+    <div className={styles.lorFont}>
+      <section className={styles.mainContent}>
+        <article className={styles.articleDesign}>
+          <div className={styles.lorHeading}>
+            <div>
+              <h2 className={styles.mainHeading}>Housing Assistance</h2>
+            </div>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/housing assistance.jpg"
+                alt="housing assistance pics"
+              />
+            </div>
+          </div>
+          <p>
+            In an era where the pursuit of global education is on the rise,
+            finding the perfect place to call home while studying abroad is a
+            crucial aspect of the student experience. Welcome to our blog, where
+            we explore the realm of housing assistance and how Axelis Overseas
+            Housing Solutions, in collaboration with leading platforms like
+            UniAcco, AmberStudent, Home & Co, Spotahome, Hello Fresh Living, and
+            Roomi, is your trusted partner in ensuring a smooth transition to
+            life in a new country.
+          </p>
+        </article>
+        <article>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <h2 className={styles.heading2}>
+                Embarking on a Home Away from Home
+              </h2>
+              <p>
+                Studying abroad brings a world of opportunities, and so does
+                finding the right accommodation. Axelis Overseas Housing
+                Solutions, in partnership with renowned student living
+                platforms, understands the significance of a comfortable living
+                space and is dedicated to assisting students in navigating the
+                intricate landscape of housing options.
+              </p>
+            </div>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Embarking on a Home Away from Home.jpg"
+                alt="Sop pics"
+              />
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Tailored Housing Solutions.png"
+                alt="Sop pics"
+              />
+            </div>
+            <div>
+              <h2 className={styles.heading2}>Tailored Housing Solutions</h2>
+              <p>
+                Just as academic aspirations differ, so do preferences in living
+                arrangements. Axelis Overseas, with the support of UniAcco,
+                AmberStudent, Home & Co, Spotahome, Hello Fresh Living, and
+                Roomi, provides tailored housing guidance, catering to both
+                undergraduate and postgraduate students. Whether it's a bustling
+                city apartment, a cozy suburban home, or a residence near
+                campus, our experts help students make informed decisions
+                aligned with their lifestyle and preferences.
+              </p>
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <h2 className={styles.heading2}>
+                A Network of Trusted Housing Partners
+              </h2>
+              <p>
+                Axelis Overseas has cultivated partnerships with reputable
+                housing providers globally, including UniAcco, AmberStudent,
+                Home & Co, Spotahome, Hello Fresh Living, and Roomi, ensuring a
+                diverse range of accommodation options. From modern apartments
+                to traditional homestays, students can choose from a curated
+                selection that meets their comfort and budget requirements.
+              </p>
+            </div>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/A Network of Trusted Housing Partners.jpg"
+                alt="Sop pics"
+              />
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Assistance in Application and Documentation.jpg"
+                alt="Sop pics"
+              />
+            </div>
+            <div>
+              <h2 className={styles.heading2}>
+                Assistance in Application and Documentation
+              </h2>
+              <p>
+                Navigating the housing application process can be overwhelming.
+                Axelis Overseas Housing Solutions, in collaboration with
+                UniAcco, AmberStudent, Home & Co, Spotahome, Hello Fresh Living,
+                and Roomi, offers dedicated assistance in preparing application
+                documents, understanding lease agreements, and providing
+                guidance on securing the ideal living space. We strive to
+                simplify the process, making your transition to a new home
+                seamless.
+              </p>
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <h2 className={styles.heading2}>
+                Financial Planning for Housing
+              </h2>
+              <p>
+                Understanding the financial aspects of accommodation is vital.
+                Axelis Overseas Housing Solutions, along with UniAcco,
+                AmberStudent, Home & Co, Spotahome, Hello Fresh Living, and
+                Roomi, offers comprehensive financial guidance, assisting
+                students in evaluating rental costs, exploring housing grants,
+                and creating realistic budget projections. Our aim is to make
+                quality housing accessible and achievable for students from
+                diverse backgrounds.
+              </p>
+            </div>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Financial Planning for Housing.jpg"
+                alt="Sop pics"
+              />
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Part-Time Housing Support.jpg"
+                alt="Sop pics"
+              />
+            </div>
+            <div>
+              <h2 className={styles.heading2}>Part-Time Housing Support</h2>
+              <p>
+                Recognizing the importance of part-time employment for students,
+                Axelis Overseas Housing Solutions, in collaboration with
+                UniAcco, AmberStudent, Home & Co, Spotahome, Hello Fresh Living,
+                and Roomi, goes beyond accommodation by assisting in finding
+                suitable part-time housing opportunities. Our network of
+                connections with local real estate agencies ensures students can
+                explore various housing options that align with their schedules
+                and adhere to local regulations.
+              </p>
+            </div>
+          </div>
+          <div className={styles.jobassistancesubImage}>
+            <div>
+              <h2 className={styles.heading2}>
+                Transitioning to a Future-Ready Career
+              </h2>
+              <p>
+                Axelis Overseas Housing Solutions believes in holistic support.
+                Beyond securing a place to live, we offer career counseling and
+                alumni support services. Our commitment is to empower students
+                to make informed choices regarding their future, ensuring a
+                fulfilling overall study abroad experience. Embark on a journey
+                with Axelis Overseas Housing Solutions and our esteemed
+                partners, where finding your ideal home abroad is not just a
+                process but a key component of your transformative student
+                experience. Explore our comprehensive housing assistance,
+                together with UniAcco, AmberStudent, Home & Co, Spotahome, Hello
+                Fresh Living, and Roomi, and open the door to a world of
+                seamless living.
+              </p>
+            </div>
+            <div>
+              <img
+                className={styles.lorMainImage}
+                src="/images/Transitioning to a Future-Ready Career.jpg"
+                alt="Sop pics"
+              />
+            </div>
+          </div>
+        </article>
+      </section>
+    </div>
+  );
+};
+
+export default HousingAssistsnce;
